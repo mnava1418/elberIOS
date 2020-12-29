cc320e20bef7631dab926f10569f063bcc2906d2
mnava1418 <mnavapena@gmail.com>
2020-12-29 15:22:25 -0600

    add constants

--- Elber/Controller/Constants.swift
+++ Elber/Controller/Constants.swift
@@ -8,5 +8,7 @@
 import Foundation
 
 struct Constants {
-    public static let HOST = "http://192.168.15.5:3000"
+    public static let HOST = "http://localhost:3000"
+    public static let TOKEN_KEY = "user_token"
+    public static let GENERIC_ERROR = "Se ha producido un error. Intenta mas tarde."
 }
