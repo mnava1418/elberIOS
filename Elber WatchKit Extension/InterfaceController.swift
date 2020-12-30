4be23e60e2134e3ba000790dc9a714a28ced39a1
mnava1418 <mnavapena@gmail.com>
2020-12-29 19:32:04 -0600

    recibe token from iOS app

--- Elber WatchKit Extension/InterfaceController.swift
+++ Elber WatchKit Extension/InterfaceController.swift
@@ -7,25 +7,35 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
 
 class InterfaceController: WKInterfaceController {
 
     @IBOutlet weak var btnElber: WKInterfaceButton!
     
+    var wcSession : WCSession!
+    
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
+        super.awake(withContext: context)
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        wcSession = WCSession.default
+        wcSession.delegate = self
+        wcSession.activate()
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
     }
     
     override func didAppear() {
+        super.didAppear()
         SocketIOController.sharedInstance.startConnection()
     }
 
@@ -37,5 +47,20 @@ class InterfaceController: WKInterfaceController {
                 }
             }
         }
+    }
+}
+
+extension InterfaceController: WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        print("session activated...")
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
+        if let token = message["token"] as? String {
+            AppController.saveToken(token: token) {
+                SocketIOController.sharedInstance.startConnection()
+            }
+        }
+        replyHandler(["ok":"ok"])
     }
 }
